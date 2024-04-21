e1630374196da37ba422584800e9ec0462d25697
Andreas Leibrock <git@leibi.net>
2024-04-21 10:25:56 +0200

    init

---
+++ src/test/java/net/leibi/pocgraphql/PocGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leibi.pocgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
