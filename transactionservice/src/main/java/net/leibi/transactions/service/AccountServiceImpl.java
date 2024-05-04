9b4e007f28e9fdec7cfb1001cfb7139c56e9b4da
Andreas Leibrock <git@leibi.net>
2024-05-04 09:14:58 +0200

    more effective getRandomAccount

--- transactionservice/src/main/java/net/leibi/transactions/service/AccountServiceImpl.java
+++ transactionservice/src/main/java/net/leibi/transactions/service/AccountServiceImpl.java
@@ -1,14 +1,6 @@
 package net.leibi.transactions.service;
 
 import io.micrometer.observation.annotation.Observed;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.leibi.transactions.datafetcher.AccountsDataFetcher;
@@ -17,58 +9,58 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
 @Observed
 public class AccountServiceImpl implements AccountService {
 
-  public static final Random random = new Random();
-  private final AccountsDataFetcher accountsDataFetcher;
+    public static final Random RANDOM = new Random();
+    private final AccountsDataFetcher accountsDataFetcher;
 
-  @Value("${application.number.accounts:1000}")
-  private int numberAccounts;
+    @Value("${application.number.accounts:1000}")
+    private int numberAccounts;
 
-  List<Account> accounts = new ArrayList<>(numberAccounts);
-  Set<Integer> alreadySeenAccounts = new HashSet<>(numberAccounts);
+    List<Account> accounts = new ArrayList<>(numberAccounts);
+    List<Integer> alreadySeenAccounts = new ArrayList<>(numberAccounts);
 
-  @Override
-  @Cacheable("accountById")
-  public Account getAccountById(String id) {
-    return accountsDataFetcher.getAccountById(id);
-  }
-
-  @Override
-  @Cacheable("randomAccountinService")
-  public Optional<Account> getRandomAccount() {
-
-    if (accounts.isEmpty()) {
-      fillAccounts();
+    @Override
+    @Cacheable("accountById")
+    public Account getAccountById(String id) {
+        return accountsDataFetcher.getAccountById(id);
     }
 
-    int index = getRandomNumber(0, numberAccounts - 1);
-    int cnt = 0;
-    while (alreadySeenAccounts.contains(index)) {
-      index = getRandomNumber(0, numberAccounts - 1);
-      cnt++;
-      if (cnt > numberAccounts) {
-        log.error("Could not find a new account");
-        return Optional.empty();
-      }
+    @Override
+    public Optional<Account> getRandomAccount() {
+
+        if (accounts.isEmpty()) {
+            fillAccounts();
+            log.info("Account list is filled now");
+        }
+
+        final int newIndex = alreadySeenAccounts.isEmpty() ? 0 : alreadySeenAccounts.getLast() + 1;
+
+        if (newIndex < accounts.size()) {
+            alreadySeenAccounts.add(newIndex);
+            return Optional.of(accounts.get(newIndex));
+        } else {
+            alreadySeenAccounts.clear();
+            return getRandomAccount();
+        }
     }
-    // log.debug("Returning account at index {}", index);
-    return Optional.of(accounts.get(index));
-  }
 
-  private void fillAccounts() {
-    log.info("Creating {} random accounts", numberAccounts);
-    accounts =
-        IntStream.range(0, numberAccounts)
-            .mapToObj(j -> accountsDataFetcher.getRandomAccount())
-            .toList();
-  }
+    private void fillAccounts() {
+        log.info("Creating {} random accounts", numberAccounts);
+        accounts =
+                IntStream.range(0, numberAccounts)
+                        .mapToObj(j -> accountsDataFetcher.getRandomAccount())
+                        .toList();
+    }
 
-  private int getRandomNumber(int min, int max) {
-    return (random.nextInt() * (max - min)) + min;
-  }
 }
